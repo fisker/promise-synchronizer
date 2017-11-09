98d653e27090e2a2f1b6eb944effc0e8a7cf9221
fisker <lionkay@gmail.com>
2017-11-09 10:09:58 +0800

    support none promise object as parameter

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var deasync = require('deasync')
 
+function isThenable(obj) {
+  return (
+    obj !== null && typeof obj === 'object' && typeof obj.then === 'function'
+  )
+}
+
 function promiseSynchronizer(promise) {
+  if (!isThenable(promise)) {
+    return promise
+  }
+
   var promiseResolved = false
   var promiseRejected = false
   var promiseValue
   var promiseReason
 
-  promise.then(
+  Promise.resolve(promise).then(
     function(value) {
       promiseResolved = true
       return (promiseValue = value)
